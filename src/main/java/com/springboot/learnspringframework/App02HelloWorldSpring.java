45e57eea3bb4a597fa150d35dd01363af06543b8
MahabirGupta <mahabirg81@gmail.com>
2023-08-28 11:32:57 +0800

    Retrieve Beans managed by Spring

--- src/main/java/com/springboot/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/springboot/learnspringframework/App02HelloWorldSpring.java
@@ -9,7 +9,13 @@ public class App02HelloWorldSpring {
     public static void main(String[] args) {
 //      1:  Launch a Spring Context or Spring Application - using @Configuration Class
       var context = new  AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-//      2:  Configure the things that we want Spring to manage - @Configuration Class
+
+//      2:  Configure the things that we want Spring to manage - @Configuration Class using HelloWorldConfiguration class for this
+//      example
+        context.getBean("name"); //Anything the is managed by Spring is called a bean
+
+//        Retrieving Beans managed by Spring
+        System.out.println(context.getBean("name"));
 
 
     }
--- src/main/java/com/springboot/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/springboot/learnspringframework/HelloWorldConfiguration.java
@@ -2,11 +2,16 @@ package com.springboot.learnspringframework;
 
 //Adding the Spring configuration
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfiguration {
 
 //    define the methods to create the spring beans in your configuration class
+    @Bean // create the name method using the @Bean notation
+    public String name(){
+        return "Mahabir";
+    }
 //    launch a spring context with the @ configuration class
 }
