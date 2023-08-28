a3f6e57c84f02ac882863ffcbbf690ff88f77b9b
MahabirGupta <mahabirg81@gmail.com>
2023-08-28 16:37:38 +0800

    Completed Application Context

--- src/main/java/com/springboot/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/springboot/learnspringframework/App02HelloWorldSpring.java
@@ -4,6 +4,8 @@ import com.springboot.learnspringframework.game.GameRunner;
 import com.springboot.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
@@ -12,10 +14,28 @@ public class App02HelloWorldSpring {
 
 //      2:  Configure the things that we want Spring to manage - @Configuration Class using HelloWorldConfiguration class for this
 //      example
-        context.getBean("name"); //Anything the is managed by Spring is called a bean
+        context.getBean("name"); //Anything that is managed by Spring is called a bean
 
-//        Retrieving Beans managed by Spring
+//        Retrieving Beans managed by Spring by passing the name attributes
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address2"));
+        System.out.println(context.getBean("person2"));
+        System.out.println(context.getBean("person3MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+
+//        Retrieving Beans managed by Spring by passing the type attributes using class
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person5Qualifier"));
+
+
+//        System.out.println();
+//        create a list of Arrays to store the getBeanDefinitionNames() Will print out all the bean names
+//        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println); //using Method reference in functional programming
+
+;
 
 
     }
--- src/main/java/com/springboot/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/springboot/learnspringframework/HelloWorldConfiguration.java
@@ -2,9 +2,15 @@ package com.springboot.learnspringframework;
 
 //Adding the Spring configuration
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+record Person (String name, int age, Address address){// record is a new feature that was introduced in JDK 16
+}
+
+record Address (String place, String city){}
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -13,5 +19,74 @@ public class HelloWorldConfiguration {
     public String name(){
         return "Mahabir";
     }
-//    launch a spring context with the @ configuration class
+
+    @Bean // create the name method using the @Bean notation
+    public int age(){
+        return 42;
+    }
+
+    @Bean
+    public Person person(){
+        var person = new Person("Anish",10, new Address("Kings Street","Melbourne"));
+//        person.age();
+//        person.name();
+        return person;
+    }
+
+    @Bean(name = "address2")
+    @Primary
+    public Address address(){
+        var place = new Address("Potong Pasir","Singapore");
+        return place;
+    }
+
+    @Bean(name = "address3")
+    @Qualifier("address3qualifier")
+    public Address address3(){
+        var place = new Address("New Bridge","Sydney");
+        return place;
+    }
+    @Bean(name = "person2")
+    public Person person2(){
+        var person = new Person("Anishkaa",7, address());
+//        person.age();
+//        person.name();
+        return person;
+    }
+
+    // Using existing beans of name and age to create a person
+
+//    1. Using method call
+    @Bean
+    public Person person3MethodCall(){
+        var person = new Person(name(),age(),address()); // call the appropriate method of the Bean.
+//        person.age();
+//        person.name();
+        return person;
+    }
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3){//instead of calling the methods we are passing the parameters
+        var person = new Person(name,age,address3); // call the appropriate method of the Bean.
+//        person.age();
+//        person.name();
+        return person;
+    }
+
+    @Bean
+    @Primary
+    public Person person4Parameters(String name, int age, Address address){//instead of calling the methods we are passing the parameters
+        var person = new Person(name,age,address); // call the appropriate method of the Bean.
+//        person.age();
+//        person.name();
+        return person;
+    }
+
+    @Bean
+    public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address){//instead of calling the methods we are passing the parameters
+        var person = new Person(name,age,address); // call the appropriate method of the Bean.
+//        person.age();
+//        person.name();
+        return person;
+    }
+
 }
